f27d4b4cdc805369e6a7e6936d94afcc6aad8eb2
Uislan Cruz <uislansk8@hotmail.com>
2026-01-24 16:12:00 -0300

    Evitar usar interface para criiar constantes, pois o ideal Ãe usar classe

---
+++ src/com/algaworks/tempo/TempoConstants.java
@@ -0,0 +1,11 @@
+package com.algaworks.tempo;
+
+public class TempoConstants {
+
+    public static final int SEGUNDOS_POR_MINUTO = 60;
+    public static final int SEGUNDOS_POR_HORA = 3600;
+
+    private TempoConstants() {
+
+    }
+}
